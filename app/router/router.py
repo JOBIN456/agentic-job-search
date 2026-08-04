4f4d642767ae6b8e3f79a7e0f838c75d8e284af8
Jobin Jose <109533232+JOBIN456@users.noreply.github.com>
2026-08-04 21:54:35 +0530

    feat(ui)template settings

---
+++ app/router/router.py
@@ -0,0 +1,5 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi.responses import JSONResponse
