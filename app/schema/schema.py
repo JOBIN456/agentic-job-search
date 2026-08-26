4abef10313feaf55f9dc36abddb3123894c8513f
Jobin Jose <109533232+JOBIN456@users.noreply.github.com>
2026-08-26 20:31:32 +0530

    feat(schema)add userschema

--- app/schema/schema.py
+++ app/schema/schema.py
@@ -1,1 +1,10 @@
 from pydantic import BaseModel
+
+class UserSchema(BaseModel):
+    id: int | None = None
+    username: str
+    password: str
+    is_staff: bool = False
+
+    class Config:
+        from_attributes = True
