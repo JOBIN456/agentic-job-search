78c833056d43e548f0262f6bfb28fca7dba871cd
Jobin Jose <109533232+JOBIN456@users.noreply.github.com>
2026-08-28 21:40:35 +0530

    fix(router):get api  for admin data

--- app/router.py
+++ app/router.py
@@ -96,3 +96,17 @@ def data_submit(user: UserSchema, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+@router_admin.get("/admin/users")
+def get_users(db: Session = Depends(get_db)):
+
+    users = db.query(User).all()
+
+    return [
+        {
+            "id": user.id,
+            "username": user.username,
+            "is_staff": user.is_staff
+        }
+        for user in users
+    ]
